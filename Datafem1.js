b01ad678a21950c75637c73f038ea4d5d0aec561
Jean Ricardo Vitório <112287680+JeanRVitorio@users.noreply.github.com>
2022-12-07 11:17:44 -0300

    Add files via upload

--- Datafem1.js
+++ Datafem1.js
@@ -84,5 +84,74 @@ let shopItemsData = [
         desc: "voce é a melhor",
         img: "roupasfem/fem15.jpg",
     },
-
+    {
+        id: "Um",
+        name: "camisetabranca",
+        price: 45,
+        desc: "Camiseta branca para criança",
+        img: "camisetabranca",
+    },
+    {
+        id: "Dois",
+        name: "Calça100%Linho",
+        price: 100,
+        desc: "Calça Leve 100% Linho",
+        img: "Calça100%Linho.jpg",
+    },
+    {
+        id: "Três",
+        name: "Roupão 100% algodão para banho",
+        price: 420,
+        desc: "roupão para banho",
+        img: "Roupão 100% algodão unissex P branco.jpg",
+    },
+    {
+        id: "Quatro",
+        name: "Camisola masculina de algodão",
+        price: 152,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "Camisola masculina de algodão confotavel.jpg",
+    },
+    {
+        id: "Cinco",
+        name: "Casaco Masculino Algodão",
+        price: 69,
+        desc: "Casaco Masculino de Algodão ",
+        img: "Casaco Masculino Algodão Forrado GG-46.jpg",
+    },
+    {
+        id: "Seis",
+        name: "Camsisa xadrez em algodão",
+        price: 200,
+        desc: "Camisa importada masculina",
+        img: "Camisa xadrez Masculna Manga curta em Algodão.jpg",
+    },
+    {
+        id: "Sete",
+        name: "Camisa Masculina Manga longa Puro Linho",
+        price: 598,
+        desc: "A camisa verde é confeccionado em linho",
+        img: "Foxton Camisa Masculina Manga longa linho.jpg",
+    },
+    {
+        id: "Oito",
+        name: "Jaqueta Masculina Stone Azul",
+        price: 479,
+        desc: "A jaqueta Azul é confeccionada em sarja leve",
+        img:"Jaqueta Masculina Stone Azul.jpg"
+    },
+    {
+        id: "Nove",
+        name: "Camisa Lilás Magnetada Manga Longa",
+        price: 150,
+        desc: "Camisa Liás Manga Longa magnetada",
+        img:"Camisa lilás Magnetada Manga Longa.jpg"
+    },
+    {
+        id: "Dez",
+        name: "Casaco Masculino Fuzz Vintage",
+        price: 453,
+        desc: "O casco marrom é um tecido encorporado com superfície atoalhado",
+        img:"Osklen Casaco Masculino Fuzz Vintage Pockets Marrom.jpg"
+    },
 ];
